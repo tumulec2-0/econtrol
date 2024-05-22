83c9d2191826d3574341bad4989ef993e81e2c2f
rudytako <adamczukmaciek@gmail.com>
2024-05-22 22:53:28 +0200

    another hotfix

--- src/models/User.ts
+++ src/models/User.ts
@@ -9,8 +9,6 @@ export interface IUser extends Document {
 const userSchema: Schema = new Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
     isCompany: { type: Boolean, default: false },
 });
 
